205946f57f50820ddc3e7376daa43b2608906e22
20essentials <s0l0estudi4r@gmail.com>
2024-10-10 14:06:38 +0200

    v0.0.1

---
+++ script.js
@@ -0,0 +1,10 @@
+console.groupCollapsed('Reference ⬇️');
+console.log(
+  'Joker card Image',
+  'https://wallpapers.com/images/hd/batman-joker-card-cv9tuqki28va3b2f.jpg'
+);
+console.log(
+  'Texture Joker',
+  'https://www.vecteezy.com/vhttps://www.vecteezy.com/vector-art/13309763-seamless-botanical-pattern-with-neon-monarch-butterflies-and-buds-of-flowersector-art/12069271-set-of-bats-silhouettes-flying'
+);
+console.groupEnd('');
